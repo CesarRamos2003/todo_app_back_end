8b815cdf1afac93c29560a331f027dcf8fe51b31
Cesar Ramos <meefbo@gmail.com>
2017-09-30 17:50:32 +0000

    1

---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,15 @@
+class TodoController < ApplicationController
+    
+    def index
+    
+    end
+
+
+    def show
+    @todo_description = "Make site not look like it's from the 90s"
+    @pomo_est = 3
+    end
+
+end
+
+    
