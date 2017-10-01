dcbe0e7b5b22f32f251d143c2b1099b6cd908ec6
Cesar Ramos <meefbo@gmail.com>
2017-10-01 18:23:48 +0000

    Somewhat functional edit

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -24,4 +24,17 @@ class TodoController < ApplicationController
     def edit
         @todo = Todo.find_by_id(params['id'])
     end    
+    
+    def update
+    
+        t = Todo.find_by_id(params['id'])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+        
+    end    
+        
+        
+        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ get 'todo/new', to: 'todo#new'
 get 'todo/create', to: 'todo#create'
 
 get 'todo/edit/:id', to: 'todo#edit'
+
+get 'todo/update/:id', to: 'todo#update'
 end
+
